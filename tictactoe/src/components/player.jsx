5d5356752a7b61a324a532736c8ce975d8d70421
NirajMourya <nirajmourya786@gmail.com>
2023-12-27 01:09:30 +0530

    Aded edit playername feature

--- tictactoe/src/components/player.jsx
+++ tictactoe/src/components/player.jsx
@@ -1,12 +1,28 @@
-export default function Player({name, symbol})
+import { useState } from "react";
+
+export default function Player({initialName, symbol})
 {
+    const [isEditing,setIsEditing] = useState(false)
+    const [playerName, setPlayerName] = useState(initialName)
+ 
+    function handleEditMode()
+    {
+        setIsEditing(() => !isEditing)
+    }
+     function handleChange(event)
+     {
+        setPlayerName(() => event.target.value)
+     }
     return (
         <li>
               <span className="player">
-                <span class="player-name">{name}</span>
+                {
+                   isEditing?(<input type="text" value={playerName} onChange={handleChange}></input>):
+                   (<span className="player-name">{playerName}</span>)                   
+                }
                 <span className="player-symbol">{symbol}</span>
               </span>
-              <button>Edit</button>
+              <button onClick={handleEditMode}>{isEditing?"Save":"Edit"}</button>
             </li>
     );
 }
